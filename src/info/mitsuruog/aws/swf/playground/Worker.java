3aba117365e9205fde39d9bcbbcbf1ad6a61ce7f
Mitsuru Ogawa <mitsuru.ogawa.jp+github@gmail.com>
2015-06-09 18:03:10 +0900

    Improve to handling when poller stopped

--- src/info/mitsuruog/aws/swf/playground/Worker.java
+++ src/info/mitsuruog/aws/swf/playground/Worker.java
@@ -1,5 +1,8 @@
 package info.mitsuruog.aws.swf.playground;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import info.mitsuruog.aws.swf.playground.HelloWorldWorkflow.GreeterActivitiesImpl;
 import info.mitsuruog.aws.swf.playground.HelloWorldWorkflow.GreeterWorkflowImpl;
 import info.mitsuruog.aws.swf.playground.HelloWorldWorkflowAsync.GreeterAsyncActivitiesImpl;
@@ -9,6 +12,8 @@ import info.mitsuruog.aws.swf.playground.HelloWorldWorkflowChild.ChildWorkflowIm
 import info.mitsuruog.aws.swf.playground.HelloWorldWorkflowChild.ParentWorkflowImpl;
 import info.mitsuruog.aws.swf.playground.HelloWorldWorkflowParallel.GreeterParallelActivitiesImpl;
 import info.mitsuruog.aws.swf.playground.HelloWorldWorkflowParallel.GreeterParallelWorkflowImpl;
+import info.mitsuruog.aws.swf.playground.HelloWorldWorkflowSplitMarge.GreeterSplitMargeActivitiesImpl;
+import info.mitsuruog.aws.swf.playground.HelloWorldWorkflowSplitMarge.GreeterSplitMargeWorkflowImpl;
 
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflow;
 import com.amazonaws.services.simpleworkflow.flow.ActivityWorker;
@@ -18,27 +23,58 @@ public class Worker {
 
 	public static void main(String[] args) throws Exception {
 
-		AmazonSimpleWorkflow swf = Config.swfFactory();
+		ConfigHelper configHelper = new ConfigHelper();
+		AmazonSimpleWorkflow swf = configHelper.swfFactory();
 		String domain = Config.DOMAIN;
 		String taskListToPoll = Config.TASK_LIST_NAME;
 		
 		// activity poller start
-		ActivityWorker activityWorker = new ActivityWorker(swf, domain, taskListToPoll);
+		final ActivityWorker activityWorker = new ActivityWorker(swf, domain, taskListToPoll);
 		activityWorker.addActivitiesImplementation(new GreeterActivitiesImpl());
 		activityWorker.addActivitiesImplementation(new GreeterAsyncActivitiesImpl());
 		activityWorker.addActivitiesImplementation(new ChildActivitiesImpl());
 		activityWorker.addActivitiesImplementation(new GreeterParallelActivitiesImpl());
+		activityWorker.addActivitiesImplementation(new GreeterSplitMargeActivitiesImpl());
 		activityWorker.start();
 		
 		// workflow poller start
-		WorkflowWorker workflowWorker = new WorkflowWorker(swf, domain, taskListToPoll);
+		final WorkflowWorker workflowWorker = new WorkflowWorker(swf, domain, taskListToPoll);
 		workflowWorker.addWorkflowImplementationType(GreeterWorkflowImpl.class);
 		workflowWorker.addWorkflowImplementationType(GreeterAsyncWorkflowImpl.class);
 		workflowWorker.addWorkflowImplementationType(ParentWorkflowImpl.class);
 		workflowWorker.addWorkflowImplementationType(ChildWorkflowImpl.class);
 		workflowWorker.addWorkflowImplementationType(GreeterParallelWorkflowImpl.class);
+		workflowWorker.addWorkflowImplementationType(GreeterSplitMargeWorkflowImpl.class);
 		workflowWorker.start();
 		
+        System.out.println("workflowとactivityが開始されました。");
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            public void run() {
+                try {
+                	activityWorker.shutdownAndAwaitTermination(1, TimeUnit.MINUTES);
+                	workflowWorker.shutdownAndAwaitTermination(1, TimeUnit.MINUTES);
+                    System.out.println("workflowとactivityを停止しました。");
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+               
+            }
+        });
+
+        System.out.println("何かキーを押すと停止します。");
+
+        try {
+            System.in.read();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.exit(0);
+
 	}
 	
 }
